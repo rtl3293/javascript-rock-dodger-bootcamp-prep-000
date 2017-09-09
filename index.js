d8acda5f08f021c44f36d127048503a3a22d2a62
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-09-09 22:49:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -128,7 +128,7 @@ function createRock(x) {
  * Finally, alert "YOU LOSE!" to the player.
  */
 function endGame() {
-  clearInterval(gameInterval)
+  clearInterval(gameInterval);
   for (let i = 0; i < ROCKS.length; i++) {
     ROCKS[i].remove()
   }
