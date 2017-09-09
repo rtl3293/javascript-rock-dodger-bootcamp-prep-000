59bbc51013a8a627a04ed6e341a55512f387e743
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-09-09 22:47:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -130,7 +130,7 @@ function createRock(x) {
 function endGame() {
   clearInterval(gameInterval)
   for (let i = 0; i < ROCKS.length; i++) {
-    ROCKS.remove()
+    ROCKS[i].remove()
   }
   window.removeEventListener('keydown', moveDodger)
 }
